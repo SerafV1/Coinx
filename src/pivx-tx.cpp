d149f3f41a45b703e77d80745bf5cc003897785a
Seraf <milerbrane@gmail.com>
2018-12-02 10:26:24 +0100

    Update pivx-tx.cpp

--- src/pivx-tx.cpp
+++ src/pivx-tx.cpp
@@ -47,10 +47,10 @@ static bool AppInitRawTx(int argc, char* argv[])
 
     if (argc < 2 || mapArgs.count("-?") || mapArgs.count("-help")) {
         // First part of help message is specific to this utility
-        std::string strUsage = _("Pivx Core pivx-tx utility version") + " " + FormatFullVersion() + "\n\n" +
+        std::string strUsage = _("Coinx Core Coinx-tx utility version") + " " + FormatFullVersion() + "\n\n" +
                                _("Usage:") + "\n" +
-                               "  pivx-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded pivx transaction") + "\n" +
-                               "  pivx-tx [options] -create [commands]   " + _("Create hex-encoded pivx transaction") + "\n" +
+                               "  Coinx-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded Coinx transaction") + "\n" +
+                               "  Coinx-tx [options] -create [commands]   " + _("Create hex-encoded Coinx transaction") + "\n" +
                                "\n";
 
         fprintf(stdout, "%s", strUsage.c_str());
